234f13da43b128dd850b2c6753f9ab046d000a0b
raj208 <rajendrajfc1021@gmail.com>
2023-04-03 01:10:40 +0530

    customizing admin panel

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import Team
-admin.site.register(Team)
+from django.utils.html import format_html
+
 
 # Register your models here.
-def __str__(self):
-    return self.first_name
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html(f"<img src= {object.photo.url} width = '40px' styles = 'border_radius:100px'")
+    
+    thumbnail.short_description = "photo"   
+    list_display = ("id", "first_name", "designation","thumbnail")    #need more than 1 value or add , 
+    list_display_links = ( "first_name","thumbnail")                         #(one element on it's on is not a tuple,
+    search_fields = ("first_name", "designation")                   #there should be more than one element)
+    list_filter = ("designation",)     
+    ordering = ("id",)  #ordering = ("-id",) for reversing order
+
+admin.site.register(Team, TeamAdmin)
