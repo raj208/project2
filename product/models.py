16a42de70af179a87f9cdb506f7788a064cd2b60
raj208 <rajendrajfc1021@gmail.com>
2023-04-17 23:29:36 +0530

    fixed size of products

--- product/models.py
+++ product/models.py
@@ -14,6 +14,8 @@ class Product(models.Model):
     photo3 = models.ImageField(upload_to='photos/%Y/%m/%d/',blank = True)
     description = RichTextField() 
 
+
+
     # def __str__(self):
     #     return self.product_name
     
